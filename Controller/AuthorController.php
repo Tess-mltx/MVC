056683de3ce77f8fa6f145b9a0906773a1c01ce8
Tess-mltx <maloteaux.stacy@gmail.com>
2024-02-26 10:40:53 +0100

    author model

---
+++ Controller/AuthorController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+class AuthorController
+{
+    public function index()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    private function getAuthors()
+    {
+        
+    }
+}
--- Model/Author.php
+++ Model/Author.php
@@ -5,4 +5,10 @@ class Author
 {
     public string $name;
     public string $description;
+
+    public function __construct(string $name, string $description)
+    {
+        $this->name = $name;
+        $this->description = $description;
+    }
 }
